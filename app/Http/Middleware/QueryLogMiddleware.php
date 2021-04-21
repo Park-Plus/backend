7cd2dac04b97616cbec79365d4ed93e6bc16e54b
Mattia <contact@mattiaeffendi.me>
2021-04-21 11:47:37 +0200

    :card_file_box: Added the query log middleware

---
+++ app/Http/Middleware/QueryLogMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class QueryLogMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        app('db')->connection()->enableQueryLog();
+        $req = $next($request);
+        $queries = DB::getQueryLog();
+        foreach($queries as $query){
+            if(isset($query["query"])){
+                Log::info('[QUERY] ' . $query["query"]);
+            }
+        }
+        return $req;
+    }
+}
